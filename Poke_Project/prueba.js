b59fbd039a0d80f592a4136c013ba84a1d038c18
Surucobe <garudahorror25@gmail.com>
2019-07-13 16:59:16 -0400

    agregando contenido dinamico falta controlarlo

--- Poke_Project/prueba.js
+++ Poke_Project/prueba.js
@@ -1,22 +1,42 @@
 const POKE_URL_REAL = 'https:pokeapi.co/api/v2/pokemon/'
 
-let usuario = []
+const usuario = []
 let intel
 
  const go = document.getElementById('pokedex')
  const $poke = document.getElementById('poke_all')
-
-//  function partner() {
-//  }
+ const $box = document.getElementById('poke_box')
 
 async function whoIsPoke(i) {
   const brook = await fetch(`${POKE_URL_REAL}${i}`)
   const cook = await brook.json()
   let name = cook.name
-  usuario.push(name)
+  usuario.push(cook)
   return name
 }
- 
+
+function registrarEquipo(poke) {
+  if(poke.types.length < 2){
+    return (
+      `<div class="pokemon_elegido">
+        <img src="${poke.sprites.front_default}" class="poke_img"></img>
+        <div class="info_pokemon">
+          <p>Name: ${poke.name}</p>
+          <p><span>Type: ${poke.types[0].type.name}</span></p>
+        </div>`
+    )
+  }else{
+    return(
+      `<div class="pokemon_elegido">
+        <img src="${poke.sprites.front_default}" class="poke_img"></img>
+        <div class="info_pokemon">
+          <p>Name: ${poke.name}</p>
+          <p><span>Type: ${poke.types[0].type.name}</span> <br> <span>Type: ${poke.types[1].type.name}</span></p>
+        </div>`
+    )
+  }
+}
+
  go.addEventListener('click', () =>{
    async function misty(){
     intel = prompt('Elige a tu compañero')
@@ -27,11 +47,28 @@ async function whoIsPoke(i) {
  })
 
  $poke.addEventListener('click', () =>{
-  if (usuario.length < 5) {
-    swal('',`Tu equipo Pokemon debe contener 6 pokemones \n\ Actualmente tienes: ${usuario.length} \n\ Restantes: ${6 - usuario.length}`, 'error')
-  }else{
-    let a = usuario.slice(0,5)
-    swal('' ,`${a}`, 'success')
-  }
+ if(usuario.length < 6){
+   swal('', `Para este reto debes tener un equipo de 6 pokemon \n\ \n\ Tu equipo contiene: ${usuario.length} pokemones`,'error')
+ }else{
+   usuario.forEach((item) => {
+    const pokeSTRING = registrarEquipo(item)
+    const html = document.implementation.createHTMLDocument()
+    html.body.innerHTML = pokeSTRING
+    $box.append(html.body.children[0])
+  })
+ }
+  
+  // if (usuario.length < 1) {
+  //   swal('',`Tu equipo Pokemon debe contener 6 pokemones \n\ Pokemones faltantes: ${6 - usuario.length}`, 'error')
+  // }else{
+  //   let a = usuario.slice(0,5)
+  //   a.forEach(i => {
+  //     if (i.types.length < 2){
+  //       console.log(`Name: ${i.name} \n\ Type: ${i.types[0].type.name}`)
+  //     }else{
+  //       console.log(`Name: ${i.name} \n\Type: ${i.types[0].type.name} / ${i.types[1].type.name}`)
+  //     }
+  //   });
+  // }
  })
   
