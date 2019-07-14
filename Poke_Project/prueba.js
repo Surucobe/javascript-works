fe153c88685bf8d40910ba2019c0e8cb6e5041f4
Surucobe <garudahorror25@gmail.com>
2019-07-14 01:40:58 -0400

    limpiando codigo

--- Poke_Project/prueba.js
+++ Poke_Project/prueba.js
@@ -10,9 +10,9 @@ let intel
 async function whoIsPoke(i) {
   const brook = await fetch(`${POKE_URL_REAL}${i}`)
   const cook = await brook.json()
-  let name = cook.name
+  // let name = cook.name
   usuario.push(cook)
-  return name
+  return cook
 }
 
 function registrarEquipo(poke) {
@@ -41,13 +41,13 @@ function registrarEquipo(poke) {
    async function misty(){
     intel = prompt('Elige a tu compañero')
     const a = await whoIsPoke(intel)
-    swal(`Pokemon: ${a}`)
+     swal(`Pokemon: ${a.name} \n\ ${a.abilities[0].ability.name}`)
    }
    misty()
  })
 
  $poke.addEventListener('click', () =>{
- if(usuario.length < 6){
+ if(usuario.length < 1){
    swal('', `Para este reto debes tener un equipo de 6 pokemon \n\ \n\ Tu equipo contiene: ${usuario.length} pokemones`,'error')
  }else{
    usuario.forEach((item) => {
@@ -57,18 +57,3 @@ function registrarEquipo(poke) {
     $box.append(html.body.children[0])
   })
  }
-  
-  // if (usuario.length < 1) {
-  //   swal('',`Tu equipo Pokemon debe contener 6 pokemones \n\ Pokemones faltantes: ${6 - usuario.length}`, 'error')
-  // }else{
-  //   let a = usuario.slice(0,5)
-  //   a.forEach(i => {
-  //     if (i.types.length < 2){
-  //       console.log(`Name: ${i.name} \n\ Type: ${i.types[0].type.name}`)
-  //     }else{
-  //       console.log(`Name: ${i.name} \n\Type: ${i.types[0].type.name} / ${i.types[1].type.name}`)
-  //     }
-  //   });
-  // }
- })
-  
