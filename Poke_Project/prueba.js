0e3c934f8959574d0b26de288068e0db4e02b9e8
Surucobe <garudahorror25@gmail.com>
2019-07-14 23:42:25 -0400

    happy little accidents

--- Poke_Project/prueba.js
+++ Poke_Project/prueba.js
@@ -3,9 +3,10 @@ const POKE_URL_REAL = 'https:pokeapi.co/api/v2/pokemon/'
 const usuario = []
 let intel
 
- const go = document.getElementById('pokedex')
- const $poke = document.getElementById('poke_all')
- const $box = document.getElementById('poke_box')
+const go = document.getElementById('pokedex')
+const $poke = document.getElementById('poke_all')
+const $box = document.getElementById('poke_box')
+const $bye = document.getElementById('poke_bye')
 
 async function whoIsPoke(i) {
   const brook = await fetch(`${POKE_URL_REAL}${i}`)
@@ -16,7 +17,7 @@ async function whoIsPoke(i) {
 }
 
 function registrarEquipo(poke) {
-  if(poke.types.length < 2){
+  if (poke.types.length < 2) {
     return (
       `<div class="pokemon_elegido">
         <img src="${poke.sprites.front_default}" class="poke_img"></img>
@@ -25,8 +26,8 @@ function registrarEquipo(poke) {
           <p><span>Type: ${poke.types[0].type.name}</span></p>
         </div>`
     )
-  }else{
-    return(
+  } else {
+    return (
       `<div class="pokemon_elegido">
         <img src="${poke.sprites.front_default}" class="poke_img"></img>
         <div class="info_pokemon">
@@ -37,23 +38,28 @@ function registrarEquipo(poke) {
   }
 }
 
- go.addEventListener('click', () =>{
-   async function misty(){
+go.addEventListener('click', () => {
+  async function misty() {
     intel = prompt('Elige a tu compañero')
     const a = await whoIsPoke(intel)
-     swal(`Pokemon: ${a.name} \n\ ${a.abilities[0].ability.name}`)
-   }
-   misty()
- })
+    swal(`Pokemon: ${a.name} \n\ ${a.abilities[0].ability.name}`)
+  }
+  misty()
+})
 
- $poke.addEventListener('click', () =>{
- if(usuario.length < 1){
-   swal('', `Para este reto debes tener un equipo de 6 pokemon \n\ \n\ Tu equipo contiene: ${usuario.length} pokemones`,'error')
- }else{
-   usuario.forEach((item) => {
-    const pokeSTRING = registrarEquipo(item)
-    const html = document.implementation.createHTMLDocument()
-    html.body.innerHTML = pokeSTRING
-    $box.append(html.body.children[0])
-  })
- }
+$poke.addEventListener('click', () => {
+  if (usuario.length < 1) {
+    swal('', `Para este reto debes tener un equipo de 6 pokemon \n\ \n\ Tu equipo contiene: ${usuario.length} pokemones`, 'error')
+  } else {
+    usuario.forEach((item) => {
+      const pokeSTRING = registrarEquipo(item)
+      const html = document.implementation.createHTMLDocument()
+      html.body.innerHTML = pokeSTRING
+      $box.append(html.body.children[0])
+    })
+  }
+})
+
+$bye.addEventListener('click', () => {
+  $box.innerHTML = ''
+})
