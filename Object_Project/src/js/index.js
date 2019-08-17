c7a273969f23f17c0a38754b0e291051726dff35
Surucobe <garudahorror25@gmail.com>
2019-08-17 16:02:01 -0400

    dandole contexto

--- Object_Project/src/js/index.js
+++ Object_Project/src/js/index.js
@@ -6,13 +6,24 @@ class Permiso {
     this.apellido = apellido,
     this.fecha = fecha
   }
+
+  crearPermiso(){
+    console.log(`${this.id},<br> ${this.license}, ${this.name}, ${this.apellido}, ${this.fecha}`)
+  }
 }
+
 const $form = document.getElementById('form')
 
-$form.addEventListener('submit', (event)=>{
+$form.addEventListener('submit', async (event)=>{
   event.preventDefault()
-  debugger
   const request = new FormData($form)
   let nombre = request.get('name')
-  console.log(nombre)
+  let license = request.get('license')
+  let lastName = request.get('apellido')
+  let id = request.get('id')
+  let fecha = request.get('fecha')
+
+  const user = await new Permiso(id, license, nombre, lastName, fecha)
+  user.crearPermiso()
+  //console.log(user)
 })
