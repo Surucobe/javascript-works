d4202233d8152e1e26df20acf379f8af29138804
Surucobe <garudahorror25@gmail.com>
2019-08-21 11:23:06 -0400

    preparando funciones

--- Object_Project/src/js/index.js
+++ Object_Project/src/js/index.js
@@ -1,27 +1,32 @@
+const $main = document.getElementById('main')
 class player{
-  constructor(id, ejeX){
-    this.id = id
-    this.ejeX = ejeX//posicion en eje X
-    this.ejeY = ejeY//posicion en eje Y
-  }
+  // constructor(id, ejeX){
+  //   this.id = id
+  //   this.ejeX = ejeX//posicion en eje X
+  //   this.ejeY = ejeY//posicion en eje Y
+  // }
 
   //metodos de movimiento
   //cambiando metodo
   movimiento(){
     window.addEventListener('keydown', (event) =>{
-      switch(event){
-        case event.keyCode == '39':
-          return this.ejeX + 10
-        case event.keyCode == '37':
-          return this.ejeX - 10
-        case event.keyCode == '40':
-          return this.ejeY - 10
-        case event.keyCode == '38':
-          return this.ejeY + 10
+      let codeNme = event.keyCode
+      switch (codeNme) {
+        // return this.ejeX + 10 derecha
+        case codeNme = 39:
+          return alert('derecha')
+          // return this.ejeX - 10 izquierda
+        case codeNme = 37:
+          return alert('izquierda')
+          // return this.ejeY - 10 abajo
+        case codeNme = 40:
+          return alert('abajo')
+          // return this.ejeY + 10 arriba
+        case codeNme = 38:
+          return alert('arriba')
       }
     })
   }
 }
 
-
-const $main = document.getElementById('main')
+const playerOne = new player
