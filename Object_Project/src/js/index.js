615ae4d8b1877fd4e3f95ab34ec743c9d4522717
Surucobe <garudahorror25@gmail.com>
2019-08-20 21:07:45 -0400

    agregando metodologia

--- Object_Project/src/js/index.js
+++ Object_Project/src/js/index.js
@@ -1,1 +1,25 @@
+class player{
+  constructor(id, ejeX){
+    this.id = id
+    this.ejeX = ejeX//posicion en eje X
+    this.ejeY = ejeY//posicion en eje Y
+  }
+
+  //metodos de movimiento
+  //primera prueba sera con diferentes metodos
+  moverDerecha(){
+
+  }
+  moverIzquierda(){
+
+  }
+  moverArriba(){
+
+  }
+  moverAbajo(){
+
+  }
+}
+
+
 const $main = document.getElementById('main')
