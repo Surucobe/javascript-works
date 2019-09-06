ce1a6d8cc0b3411783258c48666679c891051562
Surucobe <garudahorror25@gmail.com>
2019-09-06 13:24:26 -0400

    new focus

--- Object_Project/src/js/index.js
+++ Object_Project/src/js/index.js
@@ -2,35 +2,25 @@ const $main = document.getElementById('main')
 const $hide = document.getElementById('hide')
 const $overlay = document.getElementById('overlay')
 const $formas = overlay.querySelectorAll('div')
-class player{
+class Player{
   constructor(id, up, down, left, right){
     this.id = id
     this.up = up
     this.down = down
     this.left = left
     this.right = right
-    this.ejeX//posicion en eje X
-    this.ejeY//posicion en eje Y
   }
   
   //metodos de movimiento
-  movimiento(n){
-    if(n == this.up){
-      console.log(`i'm tired`)
-    }else{
-      console.log(`long way to go`)
-    }
+  movimiento(){
+    console.log(`This are my keys \n\ ${this.down}, ${this.left}, ${this.right}, ${this.up}`)
   }
 }
 
-const playerOne = new player(1, 38, 40, 37, 39)
-
-const playerTwo = new player(2, 87, 83, 65, 68)
-
 $hide.addEventListener('click', ()=>{
   alert('I Exist!')
-})
+});
 
-$formas.forEach((n) =>{
-  n.addEventListener('click', () => alert('working'))
-})
+(function gaming(){
+  playerOne = new Player(1, 38, 40, 37, 39)
+})()
