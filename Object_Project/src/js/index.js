b4fa47ea4f424fc177a7a0988e5ed77ee0cf3b63
Surucobe <garudahorror25@gmail.com>
2019-09-07 22:39:00 -0400

    FINALLY ACTUAL PROGRESS

--- Object_Project/src/js/index.js
+++ Object_Project/src/js/index.js
@@ -1,7 +1,7 @@
-const $main = document.getElementById('main')
+const $top = document.getElementById('top')
 const $hide = document.getElementById('hide')
-const $overlay = document.getElementById('overlay')
 const $formas = overlay.querySelectorAll('div')
+
 class Player{
   constructor(id, up, down, left, right){
     this.id = id
@@ -11,20 +11,14 @@ class Player{
     this.right = right
   }
   
-  //metodos de movimiento
   movimiento(){
     console.log(`This are my keys \n\ ${this.down}, ${this.left}, ${this.right}, ${this.up}`)
   }
 
-  eventFunc(){
-    console.log(`is working cause ${this.id}!`)
+  workinProgress(){
+    alert(`is working cause ${this.id}!`)
   }
 }
 
-$hide.addEventListener('click', ()=>{
-  alert('I Exist!')
-});
-
-const playerOne = new Player(1, 2, 3, 4, 5)
-
-$main.addEventListener('click', playerOne.eventFunc)
+const playerOne = new Player(1, 38, 40, 37, 39)
+$top.addEventListener('click', playerOne.workinProgress)
