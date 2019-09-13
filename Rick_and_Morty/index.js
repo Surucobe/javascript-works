72ec68612afc746936faaca3941c530bdc757fd3
Surucobe <garudahorror25@gmail.com>
2019-09-13 13:26:59 -0400

    progress

--- Rick_and_Morty/index.js
+++ Rick_and_Morty/index.js
@@ -1,16 +1,17 @@
-const URL = 'https://rickandmortyapi.com/api/'
+const IMG_TEST = '../'
 const DATA = {
   characters: "https://rickandmortyapi.com/api/character",
   locations: "https://rickandmortyapi.com/api/location",
   episodes: "https://rickandmortyapi.com/api/episode"
 }
 
-const num = parseInt(Math.random() * 100000 / 100)
-
-async function getChar(){
-  const data = await fetch(`${DATA.characters}/${num}`)
-  const char = await data.json()
-  console.log(char.id, char.name, char.status)
+function num(){
+  return parseInt(Math.random() * 10000 / 100)
 }
 
-getChar()
+async function getChar(){
+  const data = await fetch(`${DATA.characters}/${num()}`)
+  const char = await data.json()
+  console.log(`ID:${char.id} \n\Name:${char.name} \n\Status:${char.status}`)
+  console.log(char)
+}
