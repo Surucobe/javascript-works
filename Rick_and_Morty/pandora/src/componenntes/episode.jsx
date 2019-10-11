315b7be053e0e92fd857c6228b79f3f8afe7a4f6
Surucobe <garudahorror25@gmail.com>
2019-10-10 22:50:04 -0400

    trying react

--- Rick_and_Morty/pandora/src/componenntes/episode.jsx
+++ Rick_and_Morty/pandora/src/componenntes/episode.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
+import proto_logo from '../test_img/wolf.gif'
 import './estilos/episode.css'
+
+const name = "hola"
 
 class Episode extends React.Component{
   render(){
@@ -11,9 +14,47 @@ class Episode extends React.Component{
         </div>
         <div className="main_episode-personajes">
           <div className="main_episode-personajes-div">
-            <img src="" alt=""/>
+            <img src={proto_logo} alt="Proto" className="main_episode-personaje-img"/>
+            <div className="main_episode-personaje-info">
+              <span><strong>{name}</strong></span>
+              <span><strong>ID</strong></span>
+              <span><strong>Status</strong></span>
+              <span><strong>Location</strong></span>
+              <span><strong>Episode</strong></span>
+            </div>
+          </div>
+          <div className="main_episode-personajes-div">
+            <img src={proto_logo} alt="Proto" className="main_episode-personaje-img" />
+            <div className="main_episode-personaje-info">
+              <span><strong>{name}</strong></span>
+              <span><strong>ID</strong></span>
+              <span><strong>Status</strong></span>
+              <span><strong>Location</strong></span>
+              <span><strong>Episode</strong></span>
+            </div>
+          </div>
+          <div className="main_episode-personajes-div">
+            <img src={proto_logo} alt="Proto" className="main_episode-personaje-img" />
+            <div className="main_episode-personaje-info">
+              <span><strong>{name}</strong></span>
+              <span><strong>ID</strong></span>
+              <span><strong>Status</strong></span>
+              <span><strong>Location</strong></span>
+              <span><strong>Episode</strong></span>
+            </div>
+          </div>
+          <div className="main_episode-personajes-div">
+            <img src={proto_logo} alt="Proto" className="main_episode-personaje-img" />
+            <div className="main_episode-personaje-info">
+              <span><strong>{name}</strong></span>
+              <span><strong>ID</strong></span>
+              <span><strong>Status</strong></span>
+              <span><strong>Location</strong></span>
+              <span><strong>Episode</strong></span>
+            </div>
           </div>
         </div>
+        
       </div>
     )
   }
