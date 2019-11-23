25eca0d5e810cc6085cc4b71ecc69485b8be152c
Surucobe <garudahorror25@gmail.com>
2019-11-23 07:18:50 -0400

    quick push

---
+++ radio/src/Pages/Gallery.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+class Gallery extends Component {
+  render() {
+    return(
+      <p>Gallery</p>
+    )
+  }
+}
+
+export default Gallery
--- radio/src/index.js
+++ radio/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Gallery from './Pages/Gallery'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Gallery />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
