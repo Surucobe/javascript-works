5d5140d1ace03b0bbdad9ad1a708014cd0243054
Surucobe <garudahorror25@gmail.com>
2019-11-26 16:49:38 -0400

    small change

---
+++ meme_generator/src/Components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class Navbar extends from React.Component{
+
+}
+
+export default Navbar
--- meme_generator/src/index.js
+++ meme_generator/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Gallery from './Pages/Gallery'
 
+const URL = 'https://api.imgflip.com/get_memes'
+
 ReactDOM.render(<Gallery />, document.getElementById('root'));
