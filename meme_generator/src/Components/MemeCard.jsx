167c30c9abacabf908367ea33e3c9fbc4b08a4d2
Surucobe <garudahorror25@gmail.com>
2019-12-06 23:21:23 -0400

    minor change

--- meme_generator/src/Components/MemeCard.jsx
+++ meme_generator/src/Components/MemeCard.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
   class MemeCard extends React.Component{
+    sendData = e =>{
+      console.log(e)
+    }
+
     render(){
       return(
         <React.Fragment>
           {this.props.memes.map((meme) => (
-            <Link key={ meme.id } data-line={ meme.box_count } className="Meme-Card" to="/NewMeme">
+            <Link
+            onClick={ this.sendData }
+            key={ meme.id }
+            data-line={ meme.box_count }
+            className="Meme-Card"
+            to="/NewMeme">
               <img className="Actual-Meme" src={ meme.url } alt={ meme.name } />
             </Link>
           ))}
