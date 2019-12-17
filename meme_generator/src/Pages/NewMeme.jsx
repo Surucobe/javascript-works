748d11c46f0b9b104010fb6b942f8b21a61b55c2
Surucobe <garudahorror25@gmail.com>
2019-12-17 08:36:49 -0400

    testing bringing every meme to edit

--- meme_generator/src/Pages/NewMeme.jsx
+++ meme_generator/src/Pages/NewMeme.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './styles/NewMeme.css'
 
@@ -11,15 +12,36 @@ class NewMeme extends React.Component{
       },
     }
 
+    componentDidMount(){
+      const memeID = this.props.match.params.memeId
+      this.getMeme(memeID)
+    }
+
+    async getMeme(id){
+      const data = await fetch(` https://api.imgflip.com/${id}`)
+      const obj = data.json()
+      this.setState({currentMeme: obj})
+    }
+
+    handleSubmit = e =>{
+      e.preventDefault()
+    }
+
   render(){
     return(
       <React.Fragment>
         <div className="NewMeme-Container">
-          <form className="NewMeme-Form">
+          <form className="NewMeme-Form" onSubmit={this.handleSubmit}>
             <label htmlFor="">First Line</label>
             <input type="text" name="first-line" />
             <label htmlFor="">Second line</label>
             <input type="text" name="second-line" />
+            <Link className="NewMeme-Link" to="/">
+              Go Back
+            </Link>
+            <button className="Submit-button">
+              Submit
+            </button>
           </form>
           <div className="NewMeme-img">
             <figure className="edit-meme-container">
