d080e36e9e899a5f4ba897adae8d4416d3088b6e
Surucobe <garudahorror25@gmail.com>
2019-12-25 22:41:37 -0400

    i add the podcast project

---
+++ podcast/pages/index.jsx
@@ -0,0 +1,32 @@
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola Mundo!</h1>
+        <p>Bienvenidos todos al curso de Next.Js</p>
+
+        <img src="/static/platzi-logo.png" alt="Platzi Logo"/>
+
+        <style jxk>{`
+          h1 {
+            color: red;
+          }
+          :global(div p){
+            color: green;
+          }
+          img {
+            max-width: 50%;
+            display: block;
+            margin: 0 auto;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          body{
+            background-color: yellow;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
