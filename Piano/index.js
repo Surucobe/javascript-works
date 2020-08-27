5ef2cbea303c005ce7297683872035fa9baef99f
Surucobe <garudahorror25@gmail.com>
2020-08-27 15:49:56 -0400

    responsive nearly done, missing the vital info

--- Piano/index.js
+++ Piano/index.js
@@ -4,6 +4,8 @@ const $piano = document.getElementById('piano');
 //still need to add a way to stop the sounds after... or maybe not
 function tileKeySound(e) {
   const $audio = document.querySelector(`audio[data-sound="${e.keyCode}"]`);
+  //la linea de abajo esta destinada a manejar una transicion
+  const $tile = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
   if (!$audio) return;
 
@@ -16,8 +18,9 @@ window.addEventListener('keyup', tileKeySound);
 //function to make the piano work with the mouse, won't make it into a lone function cause there is no point 
 //(it was tricky you should feel proud of yourself :3)
 $piano.addEventListener('click', (e) => {
-  console.log(e.target);
   const $audio = document.querySelector(`audio[data-sound="${e.path[0].attributes[1].value}"]`);
+  //la linea de abajo esta destinada a manejar una transicion
+  const $tile = document.querySelector(`div[data-key="${e.path[0].attributes[1].value}"]`);
 
   if (!$audio) return;
 
