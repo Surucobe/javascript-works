737d4581a1374f9a05eb1e7bad8e22fcf5731544
Surucobe <garudahorror25@gmail.com>
2020-08-27 14:28:20 -0400

    corrected the way it look, now working on responsive

--- Piano/index.js
+++ Piano/index.js
@@ -1,38 +1,26 @@
-const $piano = document.getElementById('piano')
+const $piano = document.getElementById('piano');
 
 //keyboard listener function, will be added onto the window object so it it triggers every time we hit a certain key
 //still need to add a way to stop the sounds after... or maybe not
 function tileKeySound(e) {
-  const $audio = document.querySelector(`audio[data-sound="${e.keyCode}"]`)
+  const $audio = document.querySelector(`audio[data-sound="${e.keyCode}"]`);
 
-  console.log(e.keyCode)
+  if (!$audio) return;
 
-  if (!$audio) return
-
-  $audio.currentTime = 0
-  $audio.play()
-  // setTimeout(() => {
-  //   $audio.pause()
-  // }, 1500)
-
+  $audio.currentTime = 0;
+  $audio.play();
 }
 
-window.addEventListener('keyup', tileKeySound)
+window.addEventListener('keyup', tileKeySound);
 
-//function to make the piano work with the mouse, won't make it into an lone function cause there is no point 
+//function to make the piano work with the mouse, won't make it into a lone function cause there is no point 
 //(it was tricky you should feel proud of yourself :3)
 $piano.addEventListener('click', (e) => {
-  const $audio = document.querySelector(`audio[data-sound="${e.path[0].attributes[1].value}"]`)
-  if (!$audio) return
+  console.log(e.target);
+  const $audio = document.querySelector(`audio[data-sound="${e.path[0].attributes[1].value}"]`);
 
-  console.log(e.path[0].attributes[1].value)
+  if (!$audio) return;
 
-  console.log(e.target.dataset.key)
-
-  $audio.currentTime = 0
-  $audio.play()
-  // setTimeout(() => {
-  //   $audio.pause()
-  // }, 1500)
-
+  $audio.currentTime = 0;
+  $audio.play();
 })
